c5a95c1361e1635839e45ff8eb774d09bb7cd0f6
Bingshan Chang <chang@bingshan.org>
2026-04-08 04:08:46 +0000

    sly-stepper: Initial at 0.1-da84e3b
    * src/emacs-packages/manual-packages/sly-stepper/package.nix: Define
    sly-stepper from GitHub source.

---
+++ src/emacs-packages/manual-packages/sly-stepper/package.nix
@@ -0,0 +1,51 @@
+{
+  fetchgit,
+  lib,
+  sly,
+  trivialBuild,
+  ...
+}:
+let
+  inherit (lib)
+    licenses
+    maintainers
+    ;
+
+  version = "0.1";
+
+  src = fetchgit {
+    url = "https://github.com/joaotavora/sly-stepper.git";
+    rev = "da84e3bba8466c2290c2dc7c27d7f4c48c27b39e";
+    hash = "sha256-gMbmG42gxaQrgDUVySbGGpayxDM3pKAgDvYpd9KZ4B4=";
+  };
+
+  meta = {
+    description = "A portable Common Lisp stepper interface";
+    homepage = "https://github.com/joaotavora/sly-stepper";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ brsvh ];
+  };
+in
+trivialBuild rec {
+  inherit
+    meta
+    src
+    version
+    ;
+
+  pname = "sly-stepper";
+
+  buildInputs = propagatedUserEnvPkgs;
+
+  propagatedUserEnvPkgs = [
+    sly
+  ];
+
+  preBuild =
+    let
+      slyPath = "${sly}/share/emacs/site-lisp/elpa/${sly.pname}-${sly.version}";
+    in
+    ''
+      export EMACSLOADPATH="$EMACSLAODPATH:${slyPath}:${slyPath}/lib:${slyPath}/contrib"
+    '';
+}
