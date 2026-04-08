d8fca77356de1286cf0635b5bba29727caeacc49
Bingshan Chang <chang@bingshan.org>
2026-04-08 04:03:16 +0000

    consult-jinx: Initial at 0.1.0-ffc59fe
    * src/emacs-packages/manual-packages/consult-jinx/package.nix: Define
    consult-jinx package from Codeberg source.

---
+++ src/emacs-packages/manual-packages/consult-jinx/package.nix
@@ -0,0 +1,45 @@
+{
+  consult,
+  fetchgit,
+  jinx,
+  lib,
+  trivialBuild,
+  ...
+}:
+let
+  inherit (lib)
+    licenses
+    maintainers
+    ;
+
+  version = "0.1.0";
+
+  src = fetchgit {
+    url = "https://codeberg.org/bingshan/emacs-consult-jinx.git";
+    rev = "ffc59feceeb5fff14b6c84d3d24078f8333a0096";
+    hash = "sha256-aDbL3CYLG3sGJzL7x+M5iDeFa6D1ByjMR9XViybsR7Q=";
+  };
+
+  meta = {
+    description = "Consult interface for Jinx";
+    homepage = "https://codeberg.org/bingshan/emacs-consult-jinx";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ brsvh ];
+  };
+in
+trivialBuild rec {
+  inherit
+    meta
+    src
+    version
+    ;
+
+  pname = "consult-jinx";
+
+  buildInputs = propagatedUserEnvPkgs;
+
+  propagatedUserEnvPkgs = [
+    consult
+    jinx
+  ];
+}
