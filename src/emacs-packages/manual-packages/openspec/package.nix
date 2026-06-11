19e98590831f0fae23c687bd6e2c0bc5dd992751
Bingshan Chang <chang@bingshan.org>
2026-06-11 17:32:49 +0800

    openspec: Add package definition
    * src/emacs-packages/manual-packages/openspec/package.nix: Add
    package definition.

---
+++ src/emacs-packages/manual-packages/openspec/package.nix
@@ -0,0 +1,43 @@
+{
+  fetchgit,
+  lib,
+  transient,
+  trivialBuild,
+  ...
+}:
+let
+  inherit (lib)
+    licenses
+    maintainers
+    ;
+
+  version = "1.0.0";
+
+  src = fetchgit {
+    url = "https://github.com/Zacalot/openspec.el";
+    rev = "40c67b7be4077677e8fabfcf21ef3059858d5e2f";
+    hash = "sha256-W88trH4cnmepu8WeYEW2jJNDMtlg43UbeeBI7Aw8DIg=";
+  };
+
+  meta = {
+    description = "Interface for OpenSpec";
+    homepage = "https://github.com/Zacalot/openspec.el";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ brsvh ];
+  };
+in
+trivialBuild rec {
+  inherit
+    meta
+    src
+    version
+    ;
+
+  pname = "openspec";
+
+  buildInputs = propagatedUserEnvPkgs;
+
+  propagatedUserEnvPkgs = [
+    transient
+  ];
+}
