33a23cc58712d14b792e0ae6ce8eceba743586f4
Bingshan Chang <chang@bingshan.org>
2026-06-15 18:35:38 +0800

    ghostel-dwim: Add package definition
    * src/emacs-packages/manual-packages/ghostel-dwim/package.nix: Add
    manual package expression.

---
+++ src/emacs-packages/manual-packages/ghostel-dwim/package.nix
@@ -0,0 +1,47 @@
+{
+  fetchgit,
+  ghostel,
+  lib,
+  trivialBuild,
+  ...
+}:
+let
+  inherit (lib)
+    licenses
+    maintainers
+    ;
+
+  version = "0.1.0";
+
+  src = fetchgit {
+    url = "https://codeberg.org/bingshan/emacs-ghostel-dwim";
+    rev = "fc003c4165ec36aa48002f2d2483b0478d2aa591";
+    hash = "sha256-O8C6SEETdxEBVJ+2cIfMzAG/VAwDQx1pGOqv6KpJgQE=";
+  };
+
+  meta = {
+    description = "Context-aware session management commands for Ghostel";
+    homepage = "https://codeberg.org/bingshan/emacs-ghostel-dwim";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ brsvh ];
+  };
+in
+trivialBuild rec {
+  inherit
+    meta
+    src
+    version
+    ;
+
+  pname = "ghostel-dwim";
+
+  preBuild = ''
+    cp src/*.el .
+  '';
+
+  buildInputs = propagatedUserEnvPkgs;
+
+  propagatedUserEnvPkgs = [
+    ghostel
+  ];
+}
