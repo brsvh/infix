5b975a65e43128c58069be95694f7a2f1f2e9c82
Bingshan Chang <chang@bingshan.org>
2026-06-24 17:59:11 +0800

    eat-dwim: Use melpaBuild
    * src/emacs-packages/manual-packages/eat-dwim/package.nix:
    Use melpaBuild and packageRequires for eat.

--- src/emacs-packages/manual-packages/eat-dwim/package.nix
+++ src/emacs-packages/manual-packages/eat-dwim/package.nix
@@ -2,7 +2,7 @@
   eat,
   fetchgit,
   lib,
-  trivialBuild,
+  melpaBuild,
   ...
 }:
 let
@@ -26,7 +26,7 @@ let
     maintainers = with maintainers; [ brsvh ];
   };
 in
-trivialBuild rec {
+melpaBuild {
   inherit
     meta
     src
@@ -35,9 +35,7 @@ trivialBuild rec {
 
   pname = "eat-dwim";
 
-  buildInputs = propagatedUserEnvPkgs;
-
-  propagatedUserEnvPkgs = [
+  packageRequires = [
     eat
   ];
 }
