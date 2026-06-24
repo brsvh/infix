b56e9b871c6676be45578793e8b91a5b5263fa0f
Bingshan Chang <chang@bingshan.org>
2026-06-24 18:00:19 +0800

    flymake-posframe: Use melpaBuild
    * src/emacs-packages/manual-packages/flymake-posframe/package.nix:
    Use melpaBuild and packageRequires for posframe.

--- src/emacs-packages/manual-packages/flymake-posframe/package.nix
+++ src/emacs-packages/manual-packages/flymake-posframe/package.nix
@@ -2,7 +2,7 @@
   fetchgit,
   lib,
   posframe,
-  trivialBuild,
+  melpaBuild,
   ...
 }:
 let
@@ -26,7 +26,7 @@ let
     maintainers = with maintainers; [ brsvh ];
   };
 in
-trivialBuild rec {
+melpaBuild {
   inherit
     meta
     src
@@ -35,9 +35,7 @@ trivialBuild rec {
 
   pname = "flymake-posframe";
 
-  buildInputs = propagatedUserEnvPkgs;
-
-  propagatedUserEnvPkgs = [
+  packageRequires = [
     posframe
   ];
 }
