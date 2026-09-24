6587e61ac86244e3990d4803522c605f759a924d
Bingshan Chang <chang@bingshan.org>
2026-09-25 06:19:34 +0800

    infix: Remove personal formatter settings
    * .dir-locals.el: Remove Apheleia settings and retain the shared
    sentence-spacing convention.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,35 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  .
-  ((sentence-end-double-space . t)))
-
- (lisp-data-mode
-  .
-  ((eval
-    .
-    (progn
-      (setq-local apheleia-formatter 'lisp-indent)))))
-
- (markdown-ts-mode
-  .
-  ((eval
-    .
-    (progn
-      (setq-local apheleia-formatters
-                  (copy-tree apheleia-formatters))
-      (setf (alist-get 'mdformat apheleia-formatters)
-            '("mdformat"
-              "--wrap" "80"
-              "--extensions" "frontmatter"
-              "-"))
-      (setq-local apheleia-formatter 'mdformat)))))
-
- (nix-mode
-  .
-  ((apheleia-formatters . ((nixfmt "nixfmt" "--width" "50")))))
-
- (nix-ts-mode
-  .
-  ((apheleia-formatters . ((nixfmt "nixfmt" "--width" "50"))))))
+((nil . ((sentence-end-double-space . t))))
